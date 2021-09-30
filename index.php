64caae3b79fd38566951c35ced7fe5dc5bfd3729
marstonlm <marston.luann@gmail.com>
2021-09-30 13:44:18 -0700

    rough layout of portal pg

--- index.php
+++ index.php
@@ -9,5 +9,92 @@
 </head>
 
 <body>
-</body> 
+  <header>
+    <div class="header-inner">
+      <a href="index.php">
+        <img id="logo" src="images/lm3.png">
+      </a> <br />
+      <h1>Luann Marston's IT261 Portal</h1> <br />
+      <nav>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Daily</a></li>
+          <li><a href="#">Project</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Gallery</a></li>
+        </ul>
+      </nav> 
+    </div>
+    <!-- end header inner -->
+  </header>
+  <div id="wrapper">
+    <main>
+      <div id="about">
+          <h2 class="center">About Me</h2>
+          <p><img id="my-image" src="images/luann.JPEG" alt="Luann Marston">Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
+          </p>
+        </div>
+        <!-- End About -->
+
+        <hr>
+
+        <div id="mamp">
+          <h2>MAMP (XAMPP) Screenshot</h2>
+          <img src="images/xampp.png" alt="mamp screenshot">
+          <h2>Error Screenshot</h2>
+          <img src="images/error.png" alt="mamp error screenshot">
+
+        </div>
+        <!-- End mamp -->
+
+      </main>
+      <!-- End Main -->
+
+      <aside>
+        <div>
+          <h3 class="center">Weekly Exercises</h3>
+          <div>
+            <h4>Week 2</h4>
+            <ul class="week">
+              <li><a href="weeks/week2/var.php">var.php</a></li>
+              <li><a href="#">Coming Soon</a></li>
+            </ul>
+          </div>
+          <div>
+            <h4>Week 3</h4>
+            <ul class="week">
+              <li><a href="#">Coming Soon</a></li>
+              <li><a href="#">Coming Soon</a></li>
+            </ul>
+          </div>
+          <div>
+            <h4>Week 3</h4>
+            <ul class="week">
+              <li><a href="#">Coming Soon</a></li>
+              <li><a href="#">Coming Soon</a></li>
+            </ul>
+          </div>
+          
+        </div>
+      </aside>
+    <!-- End Aside -->
+
+   
+</div>
+<!-- End wrapper -->
+ <footer>
+   <div class="footer-inner">
+      <ul>
+        <li>Copyright &copy; 2021</li>
+        <li>All Rights Reserved</li>
+        <li><a href="#">Terms of use</a></li>
+        <li><a href="https://luannmarston.com">Web Design by Luann Marston</a></li>
+        <li><a href="https://validator.w3.org/">HTML Validation</a></li>
+        <li><a href="https://jigsaw.w3.org/css-validator/">CSS Validations</a></li>
+      </ul>
+</div>
+
+    </footer>
+</body>
 </html>
