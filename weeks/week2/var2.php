d8abe82f3c034d5b7e950225330b08fd91b12a7d
marstonlm <marston.luann@gmail.com>
2021-10-04 14:01:36 -0700

    var2.php video 5

--- weeks/week2/var2.php
+++ weeks/week2/var2.php
@@ -1,2 +1,84 @@
 <?php
 // More on variables, arithmetic and additional sect functions
+
+// --- Addition ---
+$a = '20';
+$b = '30';
+$c = $a + $b;
+echo $c;
+echo '<br>';
+
+// --- Addition ---
+$a = '700';
+$b = '330';
+$c = $a - $b;
+echo $c;
+echo '<br>';
+
+// --- Multiple ---
+$a = '50';
+$b = '5';
+$c = $a * $b;
+echo $c;
+echo '<br>';
+
+// --- Divide ---
+$money = 100;
+$money /= 7;
+echo $money;
+echo '<br>';
+
+// FUNCTION floor or ceiling
+$money = 100;
+$money /= 7;
+$friendly_money = number_format($money,2);
+echo $friendly_money;
+echo '<br>';
+
+// floor rounds DOWN
+$friendly_money = floor($money);
+echo $friendly_money;
+echo '<br>';
+echo '<br>';
+
+
+// ceil rounds UP
+$friendly_money = ceil($money); // rounds down
+echo $friendly_money;
+echo '<br>';
+
+// --- LOGIC and CLACULATIONS ---
+/* circumference of a circle 
+circumference = 2 pie r */
+$radius ='10';
+$pie = 3.14;
+$circumference = (2 * $pie) * $radius;
+echo $circumference;
+echo '<br>';
+
+// always think logic first
+// 22 degrees celcius = what in far?
+// far = (C * 9/5) + 32
+$celcius = 14;
+$far = ($celcius * 9/5) + 32;
+$friendly_far = floor($far);
+echo ''.$friendly_far.' degrees';
+echo '<br>';
+
+// logic and the exchange rate
+// canadian = 100
+// exchagne rate = .79
+
+echo '<br>';
+
+$canada = 1433;
+$canada *= .79;
+echo $canada;
+echo '<p>I have <b>$'.$canada.'</b> American dollars!</p>';
+echo '<br>';
+
+$canada = 1433;
+$canada *= .79;
+$friendly_canada = ceil($canada);
+echo '<p>I have <b>$'.$friendly_canada.'</b> American dollars!</p>';
+echo '<br>';
