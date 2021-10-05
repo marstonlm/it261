c493c5c036fc2b86a18c417b17925055900eb164
marstonlm <marston.luann@gmail.com>
2021-10-05 14:14:12 -0700

    Update portal page ~ still need aboutme

--- index.php
+++ index.php
@@ -4,6 +4,8 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width,initial-scale=1.0">
 <link href="css/styles.css" type="text/css" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Roboto&display=swap" rel="stylesheet"> 
 <title>Luann's Portal Page</title>
 
 </head>
@@ -14,7 +16,7 @@
       <a href="index.php">
         <img id="logo" src="images/lm3.png">
       </a> <br />
-      <h1>Luann Marston's IT261 Portal</h1> <br />
+      <h1 class="gradient">Luann Marston's IT261 Portal</h1> <br />
       <nav>
         <ul>
           <li><a href="index.php">Home</a></li>
@@ -30,70 +32,69 @@
   <div id="wrapper">
     <main>
       <div id="about">
-          <h2 class="center">About Me</h2>
-          <p><img id="my-image" src="images/luann.JPEG" alt="Luann Marston">Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
-          </p>
-        </div>
+        <h2 class="center">About Me</h2>
+        <p><img id="my-image" src="images/luann.JPEG" alt="Luann Marston">Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
+        </p>
+      </div>
         <!-- End About -->
 
-        <hr>
+      <hr>
 
-        <div id="mamp">
-          <h2>MAMP (XAMPP) Screenshot</h2>
-          <img src="images/xampp.png" alt="mamp screenshot">
+      <div id="mamp">
+        <h2>MAMP (XAMPP) Screenshot</h2>
+        <img src="images/xampp.png" alt="mamp screenshot">
 
-          <h2>Error Screenshot</h2>
-          <img src="images/error.png" alt="mamp error screenshot">
-
-        </div>
+        <h2>Error Screenshot</h2>
+        <img src="images/error.png" alt="mamp error screenshot">
+      </div>
         <!-- End mamp Assignment -->
 
+    </main>
+    <!-- End Main -->
 
-        </div>
-
-      </main>
-      <!-- End Main -->
-
-      <aside>
+    <aside>
+      <div>
+        <h3 class="center">Weekly Exercises</h3>
         <div>
-          <h3 class="center">Weekly Exercises</h3>
-          <div>
-            <h4>Week 2</h4>
-            <ul class="week">
-              <li><a href="weeks/week2/var.php">var.php</a></li>
-              <li><a href="#">Coming Soon</a></li>
-            </ul>
-          </div>
-          <hr>
-          <div>
-            <h4>Week 3</h4>
-            <ul class="week">
-              <li><a href="#">Coming Soon</a></li>
-              <li><a href="#">Coming Soon</a></li>
-            </ul>
-          </div>
-          <hr>
-          <div>
-            <h4>Week 3</h4>
-            <ul class="week">
-              <li><a href="#">Coming Soon</a></li>
-              <li><a href="#">Coming Soon</a></li>
-            </ul>
-          </div>
+          <h4>Week 2</h4>
+          <ul class="week">
+            <li><a href="weeks/week2/currency_logic.php" target="_blank">Currency Logic</a></li>
+            <li><a href="weeks/week2/currency.php" target="_blank">Currency</a></li>
+            <li><a href="weeks/week2/heredoc.php" target="_blank">heredoc</a></li>
+            <li><a href="weeks/week2/var.php" target="_blank">Var</a></li>
+            <li><a href="weeks/week2/var2.php" target="_blank">Var2</a></li>
+          </ul>
         </div>
-      </aside>
+        <hr>
+       
+        <div>
+          <h4>Week 3</h4>
+          <ul class="week">
+            <li><a href="#">Coming Soon</a></li>
+            <li><a href="#">Coming Soon</a></li>
+          </ul>
+        </div>
+        <hr>
+        <div>
+          <h4>Week 4</h4>
+          <ul class="week">
+            <li><a href="#">Coming Soon</a></li>
+            <li><a href="#">Coming Soon</a></li>
+          </ul>
+        </div>
+      </div>
+    </aside>
     <!-- End Aside -->
    
-</div>
-<!-- End wrapper -->
+  </div>
+  <!-- End wrapper -->
 
  <footer>
    <div class="footer-inner">
       <ul>
-        <li><p>Copyright &copy; 2019-2021</p></li>
-        <li><p>All Rights Reserved</p></li>
+        <li class="gradient">Copyright &copy; 2019-2021 ~ All Rights Reserved</li>
         <li><a href="#">Terms of use</a></li>
-        <li><a href="https://luannmarston.com" target="_blank">Web Design by Luann Marston</a></li>
+        <li><a href="https://luannmarston.com" target="_blank">Designed by Luann Marston</a></li>
         <li><a href="https://validator.w3.org/" target="_blank">HTML Validation</a></li>
         <li><a href="https://jigsaw.w3.org/css-validator/" target="_blank">CSS Validations</a></li>
       </ul>
