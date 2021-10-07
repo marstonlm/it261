33b090ed624dc642a823a4ff902db5473ebca032
marstonlm <marston.luann@gmail.com>
2021-10-07 13:13:06 -0700

    Created basic dislcimer file

---
+++ disclaimer.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<link href="css/styles.css" type="text/css" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Roboto&display=swap" rel="stylesheet"> 
+<title>Disclaimer</title>
+
+</head>
+
+<body>
+  <header>
+    <div class="header-inner">
+      <a href="index.php">
+        <img id="logo" src="images/lm3.png" alt="Luann Marston IT 162 home page">
+      </a> <br />
+      <h1 class="gradient">Luann Marston's IT261 Portal</h1> <br />
+      <nav>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="#">Switch</a></li>
+          <li><a href="#">Emailable form</a></li>
+          <li><a href="#">Database</a></li>
+          <li><a href="#">Gallery</a></li>
+        </ul>
+      </nav> 
+    </div>
+    <!-- end header inner -->
+  </header>
+
+  <div id="wrapper">
+    <main>
+      <h2>Disclaimer</h2>
+      <div>
+        <p class="justify-text">LuannMarston has made reasonable efforts to ensure the accuracy of the information throughout this website. However, LuannMarston reserves the right to make appropriate changes in information found on this website.
+        <br /><br />
+        Nothing contained in this website shall be construed as contractual rights. LuannMarston has many links to other websites. These include links to websites operated by other government agencies, nonprofit organizations and private businesses. LuannMarston does not endorse any content or linked materials found on websites or web services provided by and/or developed LuannMarston.
+        <br /><br />
+        When linked to another site, you are no longer on the LuannMarston website and this Privacy Notice will not apply. When linked to another website, you are subject to the privacy policy of that new site. LuannMarston does not warrant the accuracy, reliability or timeliness of any information published by this system, nor endorses any content, viewpoints, products, or services linked from this system, and shall not be held liable for any losses caused by reliance on the accuracy, reliability or timeliness of such information. Portions of such information may be incorrect or not current.
+        <br /><br />
+        Any person or entity that relies on any information obtained from this system does so at their own risk.
+        </p>
+      </div>
+
+    </main>
+    <!-- END MAIN-->
+
+  </div>
+  <!-- End Wrapper -->
+
+
+    <!-- START FOOTER -->
+    <footer>
+      <div class="footer-inner">
+        <ul>
+          <li class="gradient">Copyright &copy; 2019-2021 ~ All Rights Reserved</li>
+          <li><a href="disclaimer.php">Terms of use</a></li>
+          <li><a href="https://luannmarston.com" target="_blank">Designed by Luann Marston</a></li>
+          <li><a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fwww.luannmarston.com%2Fit261%2Findex.php" target="_blank">HTML Validation</a></li>
+          <li><a href="https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fwww.luannmarston.com%2Fit261%2Findex.php&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en" target="_blank">CSS Validations</a></li>
+        </ul>
+      </div>
+    </footer>
+  </body>
+</html>
