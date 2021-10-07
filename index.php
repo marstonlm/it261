b91aaf58d2c6c5ebe1683d48cf8f61fef1148861
marstonlm <marston.luann@gmail.com>
2021-10-07 13:12:50 -0700

    update index.php about section, links

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
   <header>
     <div class="header-inner">
       <a href="index.php">
-        <img id="logo" src="images/lm3.png">
+        <img id="logo" src="images/lm3.png" alt="Luann Marston IT 162 home page">
       </a> <br />
       <h1 class="gradient">Luann Marston's IT261 Portal</h1> <br />
       <nav>
@@ -33,8 +33,9 @@
     <main>
       <div id="about">
         <h2 class="center">About Me</h2>
-        <p><img id="my-image" src="images/luann.JPEG" alt="Luann Marston">Zombie ipsum reversus ab viral inferno, nam rick grimes malum cerebro. De carne lumbering animata corpora quaeritis. Summus brains sit​​, morbo vel maleficia? De apocalypsi gorger omero undead survivor dictum mauris. Hi mindless mortuis soulless creaturas, imo evil stalking monstra adventus resi dentevil vultus comedat cerebella viventium. Qui animated corpse, cricket bat max brucks terribilem incessu zomby. The voodoo sacerdos flesh eater, suscitat mortuos comedere carnem virus. Zonbi tattered for solum oculi eorum defunctis go lum cerebro. Nescio brains an Undead zombies. Sicut malus putrid voodoo horror. Nigh tofth eliv ingdead.
-        </p>
+        <p><img id="my-image" src="images/luann.JPEG" alt="Luann Marston"><strong>Luann Marston, <em>helping youth bring their imagination, art, and games to life for the world to see.</em></strong></p><br>
+        <p>Programming has been a hobby of mine for many years, and I recently decided that it was time to take my hobby to the next level. My goal is to show youth and the elderly that they too can code, no matter their age or abilities.</p><br>
+        <p>When my wife gets me away from the computer, I enjoy playing cards, cribbage, to be exact, and crocheting. Over the last two years, I've gained a love for gardening and volunteer at the Beacon Food Forest about once a week, unless it's raining.</p>
       </div>
         <!-- End About -->
 
@@ -60,7 +61,7 @@
           <ul class="week">
             <li><a href="weeks/week2/currency_logic.php" target="_blank">Currency Logic</a></li>
             <li><a href="weeks/week2/currency.php" target="_blank">Currency</a></li>
-            <li><a href="weeks/week2/heredoc.php" target="_blank">heredoc</a></li>
+            <li><a href="weeks/week2/heredoc.php" target="_blank">Heredoc</a></li>
             <li><a href="weeks/week2/var.php" target="_blank">Var</a></li>
             <li><a href="weeks/week2/var2.php" target="_blank">Var2</a></li>
           </ul>
@@ -93,10 +94,10 @@
    <div class="footer-inner">
       <ul>
         <li class="gradient">Copyright &copy; 2019-2021 ~ All Rights Reserved</li>
-        <li><a href="#">Terms of use</a></li>
+        <li><a href="disclaimer.php">Terms of use</a></li>
         <li><a href="https://luannmarston.com" target="_blank">Designed by Luann Marston</a></li>
-        <li><a href="https://validator.w3.org/" target="_blank">HTML Validation</a></li>
-        <li><a href="https://jigsaw.w3.org/css-validator/" target="_blank">CSS Validations</a></li>
+        <li><a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fwww.luannmarston.com%2Fit261%2Findex.php" target="_blank">HTML Validation</a></li>
+        <li><a href="https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fwww.luannmarston.com%2Fit261%2Findex.php&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en" target="_blank">CSS Validations</a></li>
       </ul>
     </div>
 
