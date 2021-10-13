69c6d0b2658cdf35de9c28c137787678d9c8fcdd
marstonlm <marston.luann@gmail.com>
2021-10-13 11:02:48 -0700

    Created comingsoon .php for future assignemnts

---
+++ comingsoon.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0">
+<link href="css/styles.css" type="text/css" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@600&family=Roboto&display=swap" rel="stylesheet"> 
+<title>Luann's Portal Page</title>
+
+</head>
+
+<body>
+  <header>
+    <div class="header-inner">
+      <a href="index.php">
+        <img id="logo" src="images/lm3.png" alt="Luann Marston IT 162 home page">
+      </a> <br />
+      <h1 class="gradient">Luann Marston's IT261 Portal</h1> <br />
+      <nav>
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="website/index.php">Switch</a></li>
+          <li><a href="comingsoon.php">Emailable form</a></li>
+          <li><a href="comingsoon.php">Database</a></li>
+          <li><a href="comingsoon.php">Gallery</a></li>
+        </ul>
+      </nav> 
+    </div>
+    <!-- end header inner -->
+  </header>
+  <div id="wrapper">
+      <div class="coming-soon">
+        <img class="coming-soon" src="images/comingsoon-icon.jpg">
+      </div>
+
+    <!-- End Main -->
+
+    <!-- End Aside -->
+   
+  </div>
+  <!-- End wrapper -->
+
+ <footer>
+   <div class="footer-inner">
+      <ul>
+        <li class="gradient">Copyright &copy; 2019-2021 ~ All Rights Reserved</li>
+        <li><a href="disclaimer.php">Terms of use</a></li>
+        <li><a href="https://luannmarston.com" target="_blank">Designed by Luann Marston</a></li>
+        <li><a href="https://validator.w3.org/nu/?doc=https%3A%2F%2Fwww.luannmarston.com%2Fit261%2Findex.php" target="_blank">HTML Validation</a></li>
+        <li><a href="https://jigsaw.w3.org/css-validator/validator?uri=https%3A%2F%2Fwww.luannmarston.com%2Fit261%2Findex.php&profile=css3svg&usermedium=all&warning=1&vextwarning=&lang=en" target="_blank">CSS Validations</a></li>
+      </ul>
+    </div>
+
+  </footer>
+</body>
+</html>
