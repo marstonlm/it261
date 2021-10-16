10abe5d6d360d1ff55ac21c076f573c36ecc41d9
marstonlm <marston.luann@gmail.com>
2021-10-16 14:53:02 -0700

    wk4 exercise form3.php

---
+++ weeks/week4/form3.php
@@ -0,0 +1,104 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Form 3 with HTML and CSS</title>
+    <style>
+      form {
+        width: 400px;
+        margin: auto;        
+      }
+
+      fieldset {
+        border: 1px solid red;
+        padding: 10px;
+      }
+
+      label, textarea {
+        display: block;
+        margin-bottom: 5px;
+      }
+      
+
+      input {
+        margin-bottom: 10px;
+      }
+
+      input[type=text], input[type=email], textarea {
+        width: 90%;
+      }
+
+      h3 {
+        color: red;
+        text-align: center;
+      }
+
+      .box {
+        width: 400px;
+        margin: 20px auto;
+        padding: 15px;
+        background: green;
+      }
+
+    </style>
+
+  </head>
+
+  <body>
+    <form action="" method="post">
+      <fieldset>
+      <label>FIRST NAME</label>
+        <input type="text" name="fname">
+
+        <label>LAST NAME</label>
+        <input type="text" name="lname">
+
+        <label>EMAIL</label>
+        <input type="email" name="email">
+        <label>COMMENTS</label>
+          <textarea name="comments"></textarea>
+        <input type="submit" value="SEND IT!!!">
+      </fieldset>
+    </form>
+     
+    <?php
+// form 3 -- adding the HTML and CSS!
+      if(isset($_POST['fname'], $_POST['lname'], $_POST['email'], $_POST['comments'])) {
+        $fname = $_POST['fname'];
+        $lname = $_POST['lname'];
+        $email = $_POST['email'];
+        $comments = $_POST['comments']; 
+
+        // nexted if/else statement
+        if(empty($fname && $lname && $email && $comments)) {
+          echo '<h3>Please fill out the fields</h3>';
+        } else {
+            // echo $fname;
+            // echo $lname;
+            // echo $email;
+            // echo $comments;
+
+            echo '
+            <div class="box">
+              <ul>
+                <li><b>First Name: '.$fname.'</b></li>
+                <li><b>Last Name: '.$lname.'</b></li>
+                <li><b>Email: '.$email.'</b></li>
+                <li><b>Comments: '.$comments.'</b></li>
+              </ul>
+            </div>
+            ';
+            
+            
+
+          } // end nexted if/else statement
+
+      } // end isset
+    ?>
+  </body>
+
+  <footer>
+
+  </footer>
+</html>
