2d6e4cc0dc7bc121d9c574ebde8d3bc9054d9853
marstonlm <marston.luann@gmail.com>
2021-10-18 14:58:41 -0700

    wk4 ex celcius form

--- weeks/week4/celcius.php
+++ weeks/week4/celcius.php
@@ -1,12 +1,3 @@
-<?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-define('THIS_PAGE', basename($_SERVER['PHP_SELF']));
-
-?>
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -16,28 +7,47 @@ define('THIS_PAGE', basename($_SERVER['PHP_SELF']));
   </head>
 
   <body>
-    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']);?>" method="post">
+    <form action="<?php echo htmlspecialchars($_SERVER['PHP_SELF']); ?>" method="post">
       <fieldset>
-        <legend>Our Celcius Form</legend>
+        <legend>Celcius Form</legend>
         <label for="cel">Enter your Celcius Value!</label>
         <input type="number" name="cel">
         <input type="submit" value="Convert it!">
+        <p><a href="">Reset!</a></p>
       </fieldset>
     </form>
 
     <?php
-      if($_SERVER['REQUEST_METHOD'== 'POST']) {
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+    define('THIS_PAGE', basename($_SERVER['PHP_SELF']));
+
+      if($_SERVER['REQUEST_METHOD']== 'POST') {
         
         if(isset($_POST['cel'])) {
           $cel = $_POST['cel'];
-          $far = ($cel * 9/5) + 32;
+          $cel_int = intval($cel);
+          $far = floor(($cel_int * 9/5) + 32);
 
             if($cel == NULL) {
-              echo 'Please fill out your Celcius Value';
+              echo '<h2>Please fill out your Celcius Value</h2>';
             } elseif($far <= 32) {
-              echo 'It is pretty cold';
-
+              echo '<p>'.$far.' degrees and it is pretty cold!</p>';
+            } elseif($far <= 40) {
+              echo '<p>'.$far.' degrees and it is not as cold.</p>';
+            } elseif($far <= 50) {
+              echo '<p>'.$far.' degrees and it is getting warmer.</p>';
+            } elseif($far <= 60) {
+              echo '<p>'.$far.' degrees and I\'m really liking it!</p>';
+            } elseif($far <= 80) {
+              echo '<p>'.$far.' degrees and I\'m going swimming!</p>';
+            } elseif($far <= 95) {
+              echo '<p>'.$far.' degrees and it\'s getting hot!</p>';
+            } else {
+              echo 'It is a cooker!!!!</p>';
             }
+            
 
         } // END isset
 
