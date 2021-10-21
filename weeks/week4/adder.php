ce11b2a4d35d32ec9741bf36f76918c643feb538
marstonlm <marston.luann@gmail.com>
2021-10-21 14:41:47 -0700

    wk4 hw adder.php working. want to tidy comments

--- weeks/week4/adder.php
+++ weeks/week4/adder.php
@@ -1,26 +1,4 @@
-<?php
-  //adder-wrong.php
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);
-  define('THIS_PAGE', basename($_SERVER['PHP_SELF']));
-
-  if (isset($_POST['num1'])){
-    $num1 = $_POST['num1'];
-    $num2 = $_POST['num2'];
-    $myTotal -= $num1 + $Num2;
-    // add closing tag for <h2></h2>
-    // corrected the values ... '.$likethis.' and removed extra spaces
-    // moved the style into the p tag
-    echo '<h2>You added '.$num1.' and '.$num2.'</h2>';
-    echo '<p style="color:red;"> and the answer is <br>
-    '.$myTotal.'!</p>';
-    echo'<p><a href="">Reset page</a></p>'; // was missing the semi-colon and closing <p> tag
-   }
-
-      //;{
-        // the php closing was at the very end of the file 
-?> 
+ 
 
 <!doctype html> <!-- Was missing the doctype at start of HTML -->
 <html>
@@ -33,20 +11,67 @@
   <body>
     <h1>Adder.php</h1> 
     <!-- removed \ added method -->
-    <form  action="" method="">
+    <form  action="" method="post"> <!-- Added post to method -->
       <!-- missling label opening tag -->
-      <label>Enter the first number:</label>
-      <input type="text" name="Num1"><br>
+      <label for="num1">Enter the first number:</label>
+      <input type="number" name="num1"><br> <!-- changed Num1 to num1 and changed type to number -->
       <!-- missing label opening tag -->
       <label>Enter the second number:
       <!-- closing tag was where the opening tag should've been .. moved it to here-->
       </label>
-    <input type="text" name="num2">
+      <input type="number" name="num2"> <!-- changed type to number -->
+    
   <br>
   <!-- missing ending " in value -->
     <input type="submit" value="Add Em!!"> 
+    <p><a href="">Reset page</a></p> <!-- Moved from php area, closing <p> tag and echo -->
     </form>
- 
+    <!-- moved php from top of page to here. This is where we've been putting it during class -->
+    <?php
+  //adder-wrong.php
+  ini_set('display_errors', 1);
+  ini_set('display_startup_errors', 1);
+  error_reporting(E_ALL);
+  define('THIS_PAGE', basename($_SERVER['PHP_SELF']));
+
+
+
+
+
+  // Added the request metod
+  if($_SERVER['REQUEST_METHOD']== 'POST') {
+
+ // Added the second $_POST for num2
+    if (isset($_POST['num1'], $_POST['num2'])) {
+      $num1 = intval($_POST['num1']); //and added inval() changed from a string to a number
+      $num2 = intval($_POST['num2']); //and added inval() changed from a string to a number
+      $myTotal = intval($num1 + $num2); // changed the N in $Num2 to a lower case n $num2 and removed - from -= and 
+      
+      // corrected the values ... '.$likethis.' and removed extra spaces
+
+      // began an if/else statment to evulate the no values entered
+      if(empty($num1 || $num2)) {
+        echo  '<h2>Please enter a number in both boxes.</h2>'; // if missing a number
+      }
+      else {
+      // moved the style into the p tag
+      //combined the two echos into one
+      // add closing tag for <h2></h2>
+      // corrected $myTotal (a string) to $myTotal_int and number
+      echo '<div class="box"><h2>You added '.$num1.' and '.$num2.'</h2>
+      <p style="color:red;"> And the answer is <br>
+      '.$myTotal.'!</p>
+      </div>';
+      }
+
+      // moved to form area echo'<p><a href="">Reset page</a></p>'; // was missing the semi-colon and closing <p> tag
+    } // END isset
+
+      //;{
+        // the php closing was at the very end of the file 
+  } // END server post
+?>
+    
 
 
   </body>
