3c8f79be37962451674eb2ee8a873486863b2c21
marstonlm <marston.luann@gmail.com>
2021-10-23 12:00:32 -0700

    changed adder2.php to adder.php

--- weeks/week4/adder.php
+++ weeks/week4/adder.php
@@ -1,79 +1,58 @@
- 
-
+<!-- Moved PHP to below form -->
 <!doctype html> <!-- Was missing the doctype at start of HTML -->
 <html>
-  <head>
-    <title>My Adder Assignment</title>
-    <style>
+<head>
+<title>My Adder Assignment</title>
+<style>
 
-    </style>
-  </head>
-  <body>
-    <h1>Adder.php</h1> 
-    <!-- removed \ added method -->
-    <form  action="" method="post"> <!-- Added post to method -->
-      <!-- missling label opening tag -->
-      <label for="num1">Enter the first number:</label>
-      <input type="number" name="num1"><br> <!-- changed Num1 to num1 and changed type to number -->
-      <!-- missing label opening tag -->
-      <label>Enter the second number:
-      <!-- closing tag was where the opening tag should've been .. moved it to here-->
-      </label>
-      <input type="number" name="num2"> <!-- changed type to number -->
-    
-  <br>
-  <!-- missing ending " in value -->
-    <input type="submit" value="Add Em!!"> 
-    <p><a href="">Reset page</a></p> <!-- Moved from php area, closing <p> tag and echo -->
-    </form>
-    <!-- moved php from top of page to here. This is where we've been putting it during class -->
-    <?php
-  //adder-wrong.php
-  ini_set('display_errors', 1);
-  ini_set('display_startup_errors', 1);
-  error_reporting(E_ALL);
-  define('THIS_PAGE', basename($_SERVER['PHP_SELF']));
+</style>
+</head>
+<body>
+<h1>Adder.php</h1> 
+<form action="" method="post"> <!-- Added post to method -->
+      <!-- missling label opening tag and for-->
+<label for="num1">Enter the first number:</label>
+<input type="number" name="num1"><br> <!-- chnaged type to number and name to 'num1' -->
+<label for="num2">Enter the second number:</label> <!-- corrected label tags and put in for -->
+<input type="number" name="num2"><br> <!-- changed type to number -->
+<input type="submit" value="Add Em!!"> <!-- missing ending " --> 
+</form>
+<?php //adder-wrong.php
+
+/* Using a Linux OS and I've been having difficulty changing the ini file 
+I'll get it figured out*/
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+define('THIS_PAGE', basename($_SERVER['PHP_SELF']));
 
 
+  if (isset($_POST['num1'], $_POST['num2'])){
+    $num1 = intval($_POST['num1']);
+    $num2 = intval($_POST['num2']);
+    $myTotal = $num1 + $num2;
 
-
-
-  // Added the request metod
-  if($_SERVER['REQUEST_METHOD']== 'POST') {
-
- // Added the second $_POST for num2
-    if (isset($_POST['num1'], $_POST['num2'])) {
-      $num1 = intval($_POST['num1']); //and added inval() changed from a string to a number
-      $num2 = intval($_POST['num2']); //and added inval() changed from a string to a number
-      $myTotal = intval($num1 + $num2); // changed the N in $Num2 to a lower case n $num2 and removed - from -= and 
-      
-      // corrected the values ... '.$likethis.' and removed extra spaces
-
-      // began an if/else statment to evulate the no values entered
-      if(empty($num1 || $num2)) {
-        echo  '<h2>Please enter a number in both boxes.</h2>'; // if missing a number
-      }
-      else {
-      // moved the style into the p tag
-      //combined the two echos into one
-      // add closing tag for <h2></h2>
-      // corrected $myTotal (a string) to $myTotal_int and number
-      echo '<div class="box"><h2>You added '.$num1.' and '.$num2.'</h2>
+    if(empty($num1 || $num2)) {
+      echo  '<h2>Please enter a number in both boxes.</h2>'; // if missing num2
+    }
+    else {
+      echo '<h2>You added '.$num1.' and '.$num2.'</h2>
       <p style="color:red;"> And the answer is <br>
-      '.$myTotal.'!</p>
-      </div>';
-      }
-
-      // moved to form area echo'<p><a href="">Reset page</a></p>'; // was missing the semi-colon and closing <p> tag
-    } // END isset
-
-      //;{
-        // the php closing was at the very end of the file 
-  } // END server post
+      '.$myTotal.'!</p>';
+      echo '<p><a href="">Reset page</a></p>';
+    }
+  } 
 ?>
-    
+<!-- line 31 --- added $_POST for num2 -->
+<!-- line 31 and 32 added intval() to change from string to number -->
+<!-- line 33 removed a - that was from $myTotal -= -->
+<!-- line 35 Nested an if/else statement -->
+<!-- line 39 changed $Num2 to $num2 -->
+<!-- line 40 moved the style tag into the <p> from line 41 -->
+<!-- line 45 changed all double quotes to single quotes -->
+<!-- line 45 added a . and removed the extra spaces around the to the " $myTotal ." -->
+<!-- line 45 removed the extra single quote after the ! -->
 
-
-  </body>
-  <!-- removed a ' that was at the end of the closing statement -->
+</body>
+<!-- Removed the ''{?> after the </html> -->
 </html>
