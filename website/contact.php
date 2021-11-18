c1ff7bf07b4596de17700c71529825c2fa0bb666
marstonlm <marston.luann@gmail.com>
2021-11-18 11:55:39 -0800

    Create contact.php for wbst

---
+++ website/contact.php
@@ -0,0 +1,17 @@
+<?php include 'includes/header.php';?>
+
+  <div id="wrapper">
+    
+    <main>
+      <h1><?php echo $headline;?></h1>
+      <p>Id java pumpkin spice, acerbic cortado pumpkin spice aged aromatic. Single origin espresso mug cinnamon mazagran, single origin bar  robusta breve froth fair trade robust. Cup macchiato and black grounds percolator single origin. Robusta cultivar redeye bar , strong crema galão instant pumpkin spice foam.</p>
+      <h2>Please fill out our form</h2>
+    </main>
+    <!-- End Main -->
+    <aside>
+      <h3>Our Aside for our Form</h3>
+
+    </aside>
+    <!-- End Aside -->
+    <!-- START FOOTER -->
+    <?php include('includes/footer.php'); ?>
